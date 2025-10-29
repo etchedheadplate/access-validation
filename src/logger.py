569359f21c7c185543a9bac17a767bf4d2c748e9
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-10-29 17:59:26 +0300

    Created logger

---
+++ src/logger.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+SERVICE_NAME = os.getenv("SERVICE_NAME", "Database")
+
+
+class PrefixFilter(logging.Filter):
+    def __init__(self, prefix: str):
+        super().__init__()
+        self.prefix = prefix
+
+    def filter(self, record: logging.LogRecord):
+        record.msg = f"[{self.prefix}] {record.msg}"
+        return True
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter("[%(levelname)s] %(asctime)s - %(message)s")
+handler.setFormatter(formatter)
+
+handler.addFilter(PrefixFilter(SERVICE_NAME))
+
+logger.addHandler(handler)
+
+logger.info("Service started")
