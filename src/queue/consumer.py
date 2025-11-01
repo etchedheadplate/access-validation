99827eba39d7f13e2dc7a713811af564a85037f3
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-11-01 16:37:00 +0300

    Removed double queue binding

--- src/queue/consumer.py
+++ src/queue/consumer.py
@@ -30,8 +30,6 @@ class RabbitMQConsumer:
         queue = await self.connection.channel.declare_queue(queue_name, durable=True)
         await queue.bind(exchange, routing_key)
 
-        await queue.bind(exchange, routing_key)
-
         logger.info(f"SUB: exchange={exchange_name}, queue={queue}, topic={routing_key}")
 
         async with queue.iterator() as queue_iter:
