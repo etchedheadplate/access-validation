0e692fc6a0a311ccaafe37bf12365c4aff9bee3b
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-11-01 10:47:33 +0300

    Updated status schemas

--- src/services/status/schemas.py
+++ src/services/status/schemas.py
@@ -6,19 +6,26 @@ from pydantic import BaseModel, Field
 class BaseStatus(BaseModel):
     request_id: str
     request_status: str
-    request_result: Annotated[str | list[str], Field(description="Request result can be a string or list of strings")]
+    request_result: Annotated[
+        str | list[str], Field(description="Request result can be a string or list of strings")
+    ] = ""
 
 
 class StatusCreatedResponse(BaseStatus):
     request_status: str = "created"
-    request_result: Annotated[str | list[str], Field(description=None)] = ""
 
 
 class StatusValidatedResponse(BaseStatus):
     request_status: str = "validated"
-    request_result: Annotated[str | list[str], Field(description=None)] = ""
 
 
 class StatusRejectedResponse(BaseStatus):
     request_status: str = "rejected"
-    request_result: Annotated[str | list[str], Field(description=None)] = ""
+
+
+class StatusDoneResponse(BaseStatus):
+    request_status: str = "done"
+
+
+class StatusUnprocessableResponse(BaseStatus):
+    request_status: str = "unprocessable"
