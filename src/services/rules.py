b4d1ae05262686b5816fbabdf48f5140843f3dd3
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-11-01 22:08:48 +0300

    Updated Rules

--- src/services/rules.py
+++ src/services/rules.py
@@ -2,17 +2,7 @@ class GroupRules:
     """User can't be a member of both groups"""
 
     CONTRADICTORY = [
-        {"Frontend Developer", "Backend Developer"},
+        {"Developer", "Owner"},
         {"ExampleGroup1", "ExampleGroup2"},
         {"ExampleGroup3", "ExampleGroup4"},
-    ]
-
-
-class PermissionRules:
-    """User from from lower group can't have any permissions of higher group"""
-
-    CONTRADICTORY = [
-        {"low": "Tester", "high": "Owner"},
-        {"low": "ExampleGroup5", "high": "ExampleGroup6"},
-        {"low": "ExampleGroup7", "high": "ExampleGroup8"},
     ]
