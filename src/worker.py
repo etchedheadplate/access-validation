6aec801102e58ca234700f26fa8518ebd4b365f5
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-11-02 20:55:28 +0300

    Added routing key condition

--- src/worker.py
+++ src/worker.py
@@ -5,6 +5,7 @@ from src.logger import logger
 from src.queue import (
     EXCHANGE_NAME,
     ROUTING_KEY_STATUS_CREATED,
+    ROUTING_KEY_STATUS_REJECTED,
     ROUTING_KEY_STATUS_VALIDATED,
     ROUTING_KEY_TASK,
     send_message,
@@ -23,8 +24,9 @@ async def process_pair(task_message: dict[str, Any], status_message: dict[str, A
     if status.is_appropriate and status.request_id == task.request_id:
         await task.validate()
         message_out = await status.process(task.is_valid, task.result)
-        await send_message(EXCHANGE_NAME, ROUTING_KEY_STATUS_VALIDATED, message_out.model_dump())
-        logger.info(f"OUT: request_id={message_out.request_id}, routing_key={ROUTING_KEY_STATUS_VALIDATED}")
+        routing_key = ROUTING_KEY_STATUS_VALIDATED if task.is_valid else ROUTING_KEY_STATUS_REJECTED
+        await send_message(EXCHANGE_NAME, routing_key, message_out.model_dump())
+        logger.info(f"OUT: request_id={message_out.request_id}, routing_key={routing_key}")
 
 
 def is_status_message(msg: dict[str, Any]) -> bool:
