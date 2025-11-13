74d14e9551092aa3a339758bd8aead0c5dfa8121
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-11-13 23:25:32 +0300

    Moved .env to Pydantic BaseSettings

--- src/config.py
+++ src/config.py
@@ -4,14 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     SERVICE_NAME: str
 
-    DB_NAME: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-
-    JWT_SECRET: str
-
     RABBITMQ_HOST: str
     RABBITMQ_PORT: int
     RABBITMQ_USER: str
