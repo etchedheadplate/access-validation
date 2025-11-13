4814655b64442889412880d02d39381aee97ad96
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-11-13 23:06:28 +0300

    Added private attributes

--- src/queue/connection.py
+++ src/queue/connection.py
@@ -12,17 +12,17 @@ class RabbitMQConnection:
         self.user = settings.RABBITMQ_USER
         self.password = settings.RABBITMQ_PASSWORD
         self.vhost = settings.RABBITMQ_VHOST
-        self.connection: AbstractRobustConnection | None = None
-        self.channel: AbstractChannel | None = None
+        self.url = f"amqp://{self.user}:{self.password}@{self.host}:{self.port}/{self.vhost}"
+        self.__connection: AbstractRobustConnection | None = None
+        self.__channel: AbstractChannel | None = None
 
     async def connect(self):
-        url = f"amqp://{self.user}:{self.password}@{self.host}:{self.port}/{self.vhost}"
-        self.connection = await aio_pika.connect_robust(url)
-        self.channel = await self.connection.channel()
-        await self.channel.set_qos(prefetch_count=1)
+        self.__connection = await aio_pika.connect_robust(self.url)
+        self.__channel = await self.__connection.channel()
+        await self.__channel.set_qos(prefetch_count=1)
 
     async def close(self):
-        if self.channel and not self.channel.is_closed:
-            await self.channel.close()
-        if self.connection and not self.connection.is_closed:
-            await self.connection.close()
+        if self.__channel and not self.__channel.is_closed:
+            await self.__channel.close()
+        if self.__connection and not self.__connection.is_closed:
+            await self.__connection.close()
